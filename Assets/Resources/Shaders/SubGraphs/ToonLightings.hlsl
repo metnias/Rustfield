4dd794f7281b37916f6cf7d40ca5fe46d4669fb1
topicular <40355580+metnias@users.noreply.github.com>
2023-07-24 20:33:30 +0900

    Attempts at fixing it

--- Assets/Resources/Shaders/SubGraphs/ToonLightings.hlsl
+++ Assets/Resources/Shaders/SubGraphs/ToonLightings.hlsl
@@ -26,37 +26,7 @@ void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 Wo
     
     half total = SumDiffuse + SumSpecular;
     // If no light touches this pixel, set the color to the main light's color
-    SumColor = total <= 0 ? MainColor : SumColor / total;
+    SumColor = total <= 0 ? MainColor : (SumColor / total);
 }
-
-/*
-void AdditionalLights_half(half Smoothness, half3 WorldPosition, half3 WorldNormal, half3 WorldView,
-    half MainDiffuse, half MainSpecular, half3 MainColor,
-    out half Diffuse, out half Specular, out half3 Color)
-{
-    Diffuse = MainDiffuse;
-    Specular = MainSpecular;
-    Color = MainColor * (MainDiffuse + MainSpecular);
-
-#ifndef SHADERGRAPH_PREVIEW
-    int pixelLightCount = GetAdditionalLightsCount();
-    for (int i = 0; i < pixelLightCount; ++i)
-    {
-        Light light = GetAdditionalLight(i, WorldPosition);
-        half NdotL = saturate(dot(WorldNormal, light.direction));
-        half atten = light.distanceAttenuation * light.shadowAttenuation;
-        half thisDiffuse = atten * NdotL;
-        half thisSpecular = LightingSpecular(thisDiffuse, light.direction, WorldNormal, WorldView, 1, Smoothness);
-        Diffuse += thisDiffuse;
-        Specular + thisSpecular;
-        Color += light.color * (thisDiffuse + thisSpecular);
-    }
-#endif
-    
-    half total = Diffuse + Specular;
-    // If no light touches this pixel, set the color to the main light's color
-    Color = total <= 0 ? MainColor : Color / total;
-}
-*/
 
 #endif
