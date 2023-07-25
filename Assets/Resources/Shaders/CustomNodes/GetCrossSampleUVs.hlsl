0b20e113d4bbbae0da3e90e0cc23d4dd059dd4af
topicular <40355580+metnias@users.noreply.github.com>
2023-07-25 16:59:26 +0900

    Created Outline PostProcess

---
+++ Assets/Resources/Shaders/CustomNodes/GetCrossSampleUVs.hlsl
@@ -0,0 +1,19 @@
+﻿void GetCrossSampleUVs_float(float4 UV, float2 TexelSize, float OffsetMultiplier,
+    out float2 UVOriginal, out float2 UVTopRight, out float2 UVBottomLeft, out float2 UVTopLeft, out float2 UVBottomRight)
+{
+    UVOriginal = UV;
+    UVTopRight = UV.xy + float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomLeft = UV.xy - float2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVTopLeft = UV.xy + float2(-TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomRight = UV.xy + float2(TexelSize.x, -TexelSize.y) * OffsetMultiplier;
+}
+
+void GetCrossSampleUVs_half(half4 UV, half2 TexelSize, half OffsetMultiplier,
+    out half2 UVOriginal, out half2 UVTopRight, out half2 UVBottomLeft, out half2 UVTopLeft, out half2 UVBottomRight)
+{
+    UVOriginal = UV;
+    UVTopRight = UV.xy + half2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomLeft = UV.xy - half2(TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVTopLeft = UV.xy + half2(-TexelSize.x, TexelSize.y) * OffsetMultiplier;
+    UVBottomRight = UV.xy + half2(TexelSize.x, -TexelSize.y) * OffsetMultiplier;
+}
---
+++ Assets/Resources/Shaders/CustomNodes/IfAnyNonZeroReturnOneElseZero.hlsl
@@ -0,0 +1,17 @@
+﻿void IfAnyNonZeroReturnOneElseZero_float(float A, float B, float C, float D, out float OUT)
+{
+    OUT = 0;
+    if (A != 0) OUT = 1;
+    if (B != 0) OUT = 1;
+    if (C != 0) OUT = 1;
+    if (D != 0) OUT = 1;
+}
+
+void IfAnyNonZeroReturnOneElseZero_half(half A, half B, half C, half D, out half OUT)
+{
+    OUT = 0;
+    if (A != 0) OUT = 1;
+    if (B != 0) OUT = 1;
+    if (C != 0) OUT = 1;
+    if (D != 0) OUT = 1;
+}
